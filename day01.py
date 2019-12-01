6298103704f4b0da93e5a7f5a466bd5c9a371592
neal-o-r <neal@n-o-r.xyz>
2019-12-01 21:10:59 +0000

    first commit

---
+++ day01.py
@@ -0,0 +1,25 @@
+
+def fuel_required_for_mass(mass: int) -> int:
+    return (mass // 3) - 2
+
+
+def recursive_fuel_required(mass: int) -> int:
+    """
+    takes a mass, and computes the fuel requires, and the fuel
+    that requires etc etc.
+    """
+    fuel = fuel_required_for_mass(mass)
+    if fuel <= 0:
+        return 0
+    return fuel + recursive_fuel_required(fuel)
+
+
+if __name__ == "__main__":
+    with open("data/day01.txt") as f:
+        lines = f.read().strip().split("\n")
+
+    total_fuel = sum(fuel_required_for_mass(int(m)) for m in lines)
+    print(total_fuel)
+
+    recursive_total_fuel = sum(recursive_fuel_required(int(m)) for m in lines)
+    print(recursive_total_fuel)
