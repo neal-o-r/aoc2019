82e61426a6a40a9f49a4f1ad2faa622eaedfc1b4
Neal Ó Riain <neal.o.riain@gmail.com>
2019-12-04 16:13:19 +0000

    Update day03.py

--- day03.py
+++ day03.py
@@ -3,7 +3,6 @@ from typing import List, Tuple, Dict
 # Define types
 Point = Tuple[int, int]
 Points = List[Point]
-PointPath = Dict[Point, int]
 Step = Tuple[str, int]
 Path = List[Step]
 
@@ -29,7 +28,7 @@ def take_step(loc: Point, step: Step) -> Points:
         return [(loc[0] - i, loc[1]) for i in range(1, length + 1)]
 
 
-def points_from_path(loc: Point, path: Path) -> PointPath:
+def points_from_path(loc: Point, path: Path) -> Points:
     """
     Turn a path into a list of points traversed on the path
     return this as a dict, mapping point to when it was passed through
@@ -40,28 +39,22 @@ def points_from_path(loc: Point, path: Path) -> PointPath:
         points.extend(segment)
         loc = segment[-1]
 
-    return {p: i for i, p in enumerate(points)}
-
-
-def intersections(points1: PointPath, points2: PointPath) -> Points:
-    p1_list = list(points1.keys())[1:]
-    p2_list = list(points2.keys())[1:]
-    return list(set(p1_list) & set(p2_list))
+    return points
+def intersections(points1: Points, points2: Points) -> Points:
+    return list(set(points1[1:]) & set(points2[1:]))
 
 
 def manhattan_distance_to_origin(pt: Point) -> int:
     return abs(pt[0]) + abs(pt[1])
 
 
-def total_steps_to_pt(
-    intersection: Point, points1: PointPath, points2: PointPath
-) -> int:
-    return points1[intersection] + points2[intersection]
+def steps_taken(pt : Point, points1 : Points, points2 : Points) -> int:
+    return points1.index(pt) + points2.index(pt)
 
 
 if __name__ == "__main__":
 
-    with open("input/day03.txt") as f:
+    with open("day03.txt") as f:
         lines = f.read().strip().split("\n")
 
     paths = [parse_input(l) for l in lines]
@@ -72,6 +65,6 @@ if __name__ == "__main__":
     min_pt = min(inters, key=manhattan_distance_to_origin)
     print(min_pt, manhattan_distance_to_origin(min_pt))
 
-    total_steps = lambda x: total_steps_to_pt(x, *points)
-    min_steps = min(inters, key=total_steps)
-    print(min_steps, total_steps(min_steps))
+    steps = lambda x: steps_taken(x, *points)
+    min_steps = min(inters, key=steps)
+    print(min_steps, steps_taken(min_steps, *points))
