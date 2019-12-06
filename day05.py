f26bef1d67c4e8f3ff9e61f0c32d53009516dacd
nealo <nealo@spotify.com>
2019-12-06 14:02:17 +0000

    day5

--- day05.py
+++ day05.py
@@ -1,70 +1,100 @@
-from typing import Tuple, List
+from typing import Tuple
 
 Modes = Tuple[int, int, int]
-Ops = List[int]
 
-op_code_params = {1 : 3, 2 : 3, 3 : 1, 4 : 1}
-
-
-def string_to_ints(text : str, delimiter : str = ',') -> list:
-    return [int(i) for i in text.split(delimiter)]
-
-
-def parse_opcode(opcode : int) -> Tuple[int, Modes, int]:
+def parse_opcode(opcode : int) -> Tuple[int, Modes]:
     op = opcode % 100
     modes = tuple(opcode // (100 * 10**i) % 10 for i in (0, 1, 2))
-    nparams = op_code_params[op]
-    return op, modes, nparams
+    return op, modes
+
+def parse_modes(i : int, modes : Modes, ops : list) -> list:
+        p1 = i + 1 if modes[0] else ops[i + 1]
+        p2 = i + 2 if modes[1] else ops[i + 2]
+        p3 = ops[i + 3]
+        return p1, p2, p3
 
 
-def get_params(i : int , ops : Ops) -> list:
-    op = ops[i]
-    if op is 1 or op is 2:
-        return ops[i + 1 : i + op_code_params[op] + 1]
-    elif op is 3 or op is 4:
-        return [ops[i + 1]]
+def update(i : int, opcode : int, modes : Modes, ops : list, input_value : int) -> list:
+
+    p1, p2, p3 = parse_modes(i, modes, ops)
+
+    if opcode is 1:
+        val = ops[p1] + ops[p2]
+        ind = p3
+        step = i + 4
+
+    elif opcode is 2:
+        val = ops[p1] * ops[p2]
+        ind = p3
+        step = i + 4
+
+    elif opcode is 3:
+        val = input_value
+        ind = p1
+        step = i + 2
+
+    elif opcode is 4:
+        print(ops[p1])
+        step = i + 2
+        val = None
+        ind = None
+
+    elif opcode is 5:
+        if ops[p1] is 0:
+            step = i + 3
+        else:
+            step = ops[p2]
+        val = None
+        ind = None
+
+    elif opcode is 6:
+        if ops[p1] != 0:
+            step = i + 3
+        else:
+            step = ops[p2]
+        val = None
+        ind = None
+
+    elif opcode is 7:
+        if ops[p1] < ops[p2]:
+            val = 1
+            ind = p3
+        else:
+            val = 0
+            ind = p3
+        step = i + 4
+
+    elif opcode is 8:
+        if ops[p1] == ops[p2]:
+            val = 1
+            ind = p3
+        else:
+            val = 0
+            ind = p3
+        step = i + 4
+
+    return step, [o if i != ind else val for i, o in enumerate(ops)]
 
 
-def parse_modes(params : list, modes : Modes, ops : Ops) -> list:
-    pout = []
-    for p, m in zip(params, modes):
-        if m is 0:
-            pout.append(ops[p])
-        elif p is 1:
-            pout.append(p)
-    return pout
+def run_intcode(i, ops, input_value = 1):
+    opcode, modes = parse_opcode(ops[i])
 
-
-def update(i : int, ops : Ops, input_val : int) -> Ops:
-    op, modes, _ = parse_opcode(ops[i])
-    if op is 1:
-        params = parse_modes(get_params(i, ops), modes, ops)
-        print(params)
-        val = params[0] + params[1]
-        ind = params[2]
-    elif op is 2:
-        params = parse_modes(get_params(i, ops), modes, ops)
-        val = params[0] * params[1]
-        ind = params[2]
-    elif op is 3:
-        val = input_val
-        ind = get_params(i, ops)[0]
-    elif op is 4:
-        print(get_params(i, ops)[0])
-
-    print(val, ind)
-    return [v if i != ind else val for i, v in enumerate(ops)]
-
-
-def run(i : int, ops : Ops, input_val : int = 1) -> Ops:
-    op, _, nparams = parse_opcode(ops[i])
-    if op is 99:
+    if opcode == 99:
         return ops
-    return run(i + nparams + 1, update(i, ops, input_val))
+    i, ops = update(i, opcode, modes, ops, input_value)
+    return run_intcode(i, ops, input_value)
+
+
 
 
 if __name__ == "__main__":
-    with open("input/day05.txt") as f:
-        text = f.read().strip()
 
-    ops = string_to_ints(text)
+    with open('input/day05.txt', 'r') as f:
+        program = [int(x) for x in f.read().split(',')]
+
+    run_intcode(0, program, 1)
+
+    with open('input/day05.txt', 'r') as f:
+        program = [int(x) for x in f.read().split(',')]
+
+    run_intcode(0, program, 5)
