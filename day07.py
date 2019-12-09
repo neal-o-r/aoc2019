96146e4f899672a42466396c8e00599a8ad0bb82
neal-o-r <neal@n-o-r.xyz>
2019-12-09 22:24:57 +0000

    day9

--- day07.py
+++ day07.py
@@ -1,74 +1,58 @@
-from day05 import update, parse_opcode
-from typing import Iterator
 from itertools import permutations
 
+def run(program, program_input):
+    instruction_sizes = [0, 4, 4, 2, 2, 3, 3, 4, 4]
+    ip = 0
+    while True:
+        op = program[ip]
+        modes = [(op // 10 ** i) % 10 for i in range(2, 5)]
+        op = op % 100
+        if op == 99:
+            return
+        size = instruction_sizes[op]
+        args = [program[ip + i] for i in range(1, size)]
+        params = [x if modes[i] else program[x]
+            for i, x in enumerate(args)]
+        ip += size
+        if op == 1:
+            program[args[2]] = params[0] + params[1]
+        elif op == 2:
+            program[args[2]] = params[0] * params[1]
+        elif op == 3:
+            program[args[0]] = program_input.pop(0)
+        elif op == 4:
+            yield params[0]
+        elif op == 5:
+            if params[0]:
+                ip = params[1]
+        elif op == 6:
+            if not params[0]:
+                ip = params[1]
+        elif op == 7:
+            program[args[2]] = int(params[0] < params[1])
+        elif op == 8:
+            program[args[2]] = int(params[0] == params[1])
 
-def run_intcode(i: int, ops: list, input_iter: Iterator, output: list = []) -> list:
-    opcode, modes = parse_opcode(ops[i])
-
-    if opcode == 99:
-        return i, ops, output
-
-    i, ops, output = update(i, opcode, modes, ops, input_iter)
-    return run_intcode(i, ops, input_iter, output)
-
-
-def run_intcode_halt(i: int, ops: list, input_iter: Iterator, output: list = []) -> list:
-    opcode, modes = parse_opcode(ops[i])
-
-    if opcode == 99:
-        return i, ops, output, True
-    if opcode == 4:
-        i, ops, output = update(i, opcode, modes, ops, input_iter)
-        return i, ops, output, False
-
-    i, ops, output = update(i, opcode, modes, ops, input_iter)
-    return run_intcode_halt(i, ops, input_iter, output)
-
-
-def run_sequence(settings : list, ops: list) -> list:
-    output = [0]
-    for s in settings:
-        _, _ , o = run_intcode(0, ops, iter([s, output[-1]]))
-        output += o
-
-    return output
-
-
-def run_feedback(settings : list, ops: list) -> list:
-    output = [0]
-    op_out = []
-    i_out = []
-    for s in settings:
-        i, op, o = run_intcode(0, ops, iter([s, output[-1]]))
-        output += o
-        op_out.append(op)
-        i_out.append(i)
-
-    c = 0
-    halts = [False] * len(settings)
-    inputs = [[o_i, s_i] for o_i, s_i in zip(output, settings)]
-
-    while not halts[-1]:
-
-        i, op, o, halt = run_intcode_halt(i_out[c], op_out[c], iter(inputs[c]))
-        halts[c % 5] = halt
-        output += o
-        op_out[c % 5] = op
-        c = (c + 1) % 5
-
-    return output
+def run_amplifiers(program, phases):
+    inputs = [[x] for x in phases]
+    inputs[0].append(0)
+    amplifiers = [run(program, x) for x in inputs]
+    while True:
+        for i, a in enumerate(amplifiers):
+            try:
+                x = next(a)
+            except StopIteration:
+                return x
+            inputs[(i + 1) % len(inputs)].append(x)
 
 
 if __name__ == "__main__":
 
     with open("input/day07.txt") as f:
-        program = [int(x) for x in f.read().split(",")]
+        program = list(map(int, f.read().split(',')))
 
-    settings = list(range(0, 5))
-    program = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
-    settings = [9,8,7,6,5]
-    # perms = list(permutations(settings))
-    # thrusts = [run_sequence(sp, program)[-1] for sp in perms]
-    # print("Max: ", max(thrusts))
+    print(max(run_amplifiers(program, phases)
+        for phases in permutations(range(5))))
 
+    print(max(run_amplifiers(program, phases)
+        for phases in permutations(range(5, 10))))
---
+++ day09.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+from typing import List, Generator, Tuple, Dict
+
+Program = List[int]
+Input = List[int]
+Output = Generator[int, int, int]
+Modes = List[int]
+Memory = Dict[int, int]
+
+
+nargs = {0: 0, 1: 4, 2: 4, 3: 2, 4: 2,
+         5: 3, 6: 3, 7: 4, 8: 4, 9: 2}
+
+
+def parse_opcode(opcode: int) -> Tuple[int, Modes]:
+    op = opcode % 100
+    modes = [(opcode // 10 ** i) % 10 for i in range(2, 5)]
+    return op, modes
+
+
+def intcode(program: Program, program_input: Input) -> Output:
+
+    i, offset = 0, 0
+    memory = defaultdict(int, enumerate(program))
+    while True:
+        op, modes = parse_opcode(memory[i])
+        if op is 99:
+            return
+        size = nargs[op]
+        args = [memory[i + x] for x in range(1, size)]
+
+        reads = [(memory[x], x, memory[x + offset])[m]
+                    for x, m in zip(args, modes)]
+        write = [(x, None, x + offset)[m] for x, m in zip(args, modes)]
+
+        i += size
+        if op is 1:
+            memory[write[2]] = reads[0] + reads[1]
+        elif op is 2:
+            memory[write[2]] = reads[0] * reads[1]
+        elif op is 3:
+            memory[write[0]] = program_input.pop(-1)
+        elif op is 4:
+            yield reads[0]
+        elif op is 5 and reads[0]:
+            i = reads[1]
+        elif op is 6 and not reads[0]:
+            i = reads[1]
+        elif op is 7:
+            memory[write[2]] = int(reads[0] < reads[1])
+        elif op is 8:
+            memory[write[2]] = int(reads[0] == reads[1])
+        if op is 9:
+            offset += reads[0]
+
+
+if __name__ == "__main__":
+
+    with open("input/day09.txt") as f:
+        program = [int(x) for x in f.read().split(",")]
+
+    print(list(intcode(program, [1])))
+    print(list(intcode(program, [2])))
+
+
