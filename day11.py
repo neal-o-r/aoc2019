1df3146815fef555c01757719e91f42fd28987b0
nealo <nealo@spotify.com>
2019-12-12 13:53:51 +0000

    day 11

--- day11.py
+++ day11.py
@@ -1,19 +1,46 @@
 from day09 import intcode
 from toolz import take
-from typing import List
+from typing import List, Generator
+from collections import defaultdict
 
-Program = List[int]
-Point = List[int]
+N, S, E, W = (0, -1), (0, 1), (1, 0), (-1, 0)
+L = {N: W, E: N, S: E, W: S}
+R = {N: E, E: S, S: W, W: N}
 
+def print_grid(grid):
+    pts = list(zip(*grid))
+    xmax, xmin = max(pts[0]) + 1, min(pts[0])
+    ymax, ymin = max(pts[1]) + 1, min(pts[1])
 
-def get_step(program: Program, inp: list) -> Point:
-    return list(take(2, intcode(program, inp)))
+    for y in range(ymin, ymax):
+        for x in range(xmin, xmax):
+            c = '#' if grid[(x, y)] else ' '
+            print(c, end='')
 
+        print()
 
 
 if __name__ == "__main__":
 
     with open("input/day11.txt") as f:
         program = [int(x) for x in f.read().strip().split(",")]
+
+    inp = []
+    intgen = intcode(program, inp)
+    grid = defaultdict(int)
+    pos = (0, 0)
+    facing = N
+
+    grid[pos] = 1
+    while True:
+        inp.append(grid[pos])
+        try:
+            color = next(intgen)
+            direction = next(intgen)
+            grid[pos] = color
+            facing = R[facing] if direction else L[facing]
+            pos = (pos[0] + facing[0], pos[1] + facing[1])
+        except StopIteration:
+            break
 
 
